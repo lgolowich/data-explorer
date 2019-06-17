9a0e3a6fcc9023dd83ce640bc5ca8cbd763b0880
Louis Golowich <lgolowich@google.com>
2019-06-17 14:57:58 -0700

    Implemented new api, but need to rerun swagger-codegen

--- api/data_explorer/controllers/facets_controller.py
+++ api/data_explorer/controllers/facets_controller.py
@@ -75,6 +75,23 @@ def _process_extra_facets(extra_facets):
     current_app.config['EXTRA_FACET_INFO'] = facets
 
 
+def _get_time_series_params(ts_value_names, ts_values):
+    srt = sorted([(ts_value_names[i], i) for i in ts_value_names])
+    ts_value_names_list = [interval for value, interval in srt]
+    for i in range(len(srt)):
+        value, interval = srt[i]
+        ts_value_names[interval] = i
+    ts_value_counts_array = []
+    for entry in ts_values:
+        value_names = entry[0]
+        value_counts = entry[1]
+        entry_array = [0 for name in ts_value_names_list]
+        for i in range(len(value_names)):
+            entry_array[ts_value_names[value_names[i]]] = value_counts[i]
+        ts_value_counts_array.append(entry_array)
+    return ts_value_names_list, ts_value_counts_array
+
+
 def facets_get(filter=None, extraFacets=None):  # noqa: E501
     """facets_get
     Returns facets. # noqa: E501
@@ -108,6 +125,8 @@ def facets_get(filter=None, extraFacets=None):  # noqa: E501
     ts_ui_name = ""
     ts_description = ""
     ts_field_type = ""
+    ts_time_names = []
+    ts_value_names = {}
     ts_values = []
 
     for es_field_name, facet_info in combined_facets.iteritems():
@@ -115,42 +134,51 @@ def facets_get(filter=None, extraFacets=None):  # noqa: E501
             (not facet_info.get('is_time_series'))
                 or '.'.join(es_field_name.split('.')[:-1]) != ts_field_name):
             # No more values in this time series, so add element to facets
+            value_names, time_series_value_counts = _get_time_series_params(
+                ts_value_names, ts_values)
             facets.append(
                 Facet(
                     name=ts_ui_name,
                     description=ts_description,
                     es_field_name=ts_field_name,
                     es_field_type=ts_field_type,
-                    values=[],
-                    time_series_values=ts_values))
+                    value_names=value_names,
+                    value_counts=[],
+                    time_names=ts_time_names,
+                    time_series_value_counts=time_series_value_counts))
             ts_field_name = ""
+            ts_time_names = []
+            ts_value_names = {}
             ts_values = []
 
-        values = []
         facet = facet_info.get('es_facet')
+        value_names = []
+        value_counts = []
         for value_name, count, _ in es_response_facets[es_field_name]:
             if elasticsearch_util.is_histogram_facet(facet):
                 # For histograms, Elasticsearch returns:
                 #   name 10: count 15     (There are 15 people aged 10-19)
                 #   name 20: count 33     (There are 33 people aged 20-29)
                 # Convert "10" -> "10-19".
-                value_name = elasticsearch_util.number_to_range(
+                interval_name = elasticsearch_util.number_to_range(
                     value_name, _get_bucket_interval(facet))
             else:
                 # elasticsearch-dsl returns boolean field keys as 0/1. Use the
                 # field's 'type' to convert back to boolean, if necessary.
                 if facet_info['type'] == 'boolean':
-                    value_name = bool(value_name)
-            values.append(FacetValue(name=value_name, count=count))
+                    interval_name = bool(interval_name)
+            value_names.append(interval_name)
+            value_counts.append(count)
+            if facet_info.get('is_time_series'):
+                ts_value_names[interval_name] = value_name
 
         if facet_info.get('is_time_series'):
             ts_field_name = '.'.join(es_field_name.split('.')[:-1])
             ts_ui_name = facet_info.get('ui_facet_name')
             ts_description = facet_info.get('description')
             ts_field_type = facet_info.get('type')
-            ts_values.append(
-                TimeSeriesFacetValue(
-                    time=int(es_field_name.split('.')[-1]), values=values))
+            ts_time_names.append(int(es_field_name.split('.')[-1]))
+            ts_values.append([value_names, value_counts])
         else:
             facets.append(
                 Facet(
@@ -158,20 +186,28 @@ def facets_get(filter=None, extraFacets=None):  # noqa: E501
                     description=facet_info.get('description'),
                     es_field_name=es_field_name,
                     es_field_type=facet_info.get('type'),
-                    values=values,
-                    time_series_values=[]))
+                    value_names=value_names,
+                    value_counts=value_counts,
+                    time_names=[],
+                    time_series_value_counts=[]))
 
     if ts_field_name:
         # Finish up last time series facet if necessary
+        value_names, time_series_value_counts = _get_time_series_params(
+            ts_value_names, ts_values)
         facets.append(
             Facet(
                 name=ts_ui_name,
                 description=ts_description,
                 es_field_name=ts_field_name,
                 es_field_type=ts_field_type,
-                values=[],
-                time_series_values=ts_values))
+                value_names=value_names,
+                value_counts=[],
+                time_names=ts_time_names,
+                time_series_value_counts=time_series_value_counts))
         ts_field_name = ""
+        ts_time_names = []
+        ts_value_names = {}
         ts_values = []
 
     return FacetsResponse(
