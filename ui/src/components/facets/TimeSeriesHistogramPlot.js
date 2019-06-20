60f3a3af0662931ef15d59283d90697d701493b4
Louis Golowich <lgolowich@google.com>
2019-06-20 14:05:51 -0700

    Redid time series histogram plot in one vega visualization

--- ui/src/components/facets/TimeSeriesHistogramPlot.js
+++ ui/src/components/facets/TimeSeriesHistogramPlot.js
@@ -1,39 +1,32 @@
 import React, { Component } from "react";
 import classNames from "classnames";
-import Grid from '@material-ui/core/Grid';
+import { Handler } from "vega-tooltip";
+import * as vl from "vega-lite";
+import Vega from "react-vega";
 import { withStyles } from "@material-ui/core/styles";
 
+import "./HistogramFacet.css";
 import * as Style from "libs/style";
 import colors from "libs/colors";
 import FacetHeader from "components/facets/FacetHeader";
-import HistogramPlot from "components/facets/HistogramPlot";
-import TimeSeriesPlotHeader from "components/facets/TimeSeriesPlotHeader";
 
 const styles = {
   timeSeriesHistogramPlot: {
     overflowX: "visible",
     overflowY: "auto"
   },
-  grid: {
-    flexWrap: "nowrap"
-  },
-  gridItem: {
-    paddingRight: "4px"
-  },
-  histogramPlot: {
-    padding: "5px"
+  vega: {
+    textAlign: "center"
   }
 };
 
-function getValues(value_names, value_counts) {
-  let values = []
-  for (let i = 0; i < value_names.length; i++) {
-    values.push({
-      name: value_names[i],
-      count: value_counts[i]
-    });
-  }
-  return values;
+// If more than 120px, facet value name will be cut off with "..."
+const facetValueNameWidthLimit = 120;
+
+function isCategorical(es_field_type) {
+  return (
+    es_field_type === "text" || es_field_type === "samples_overview"
+  );
 }
 
 // From https://stackoverflow.com/questions/39342575
@@ -45,55 +38,223 @@ function maxCount(arr) {
 class TimeSeriesHistogramPlot extends Component {
   constructor(props) {
     super(props);
+    this.isValueDimmed = this.isValueDimmed.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onNewView = this.onNewView.bind(this);
   }
 
   render() {
     const { classes } = this.props;
 
-    const gridItems = []
-    for (var ti = 0; ti < this.props.facet.time_names.length; ti++) {
-      let tsv_es_field_name = (this.props.facet.es_field_name + "." +
-			       this.props.facet.time_names[ti]);
-      let values = getValues(this.props.facet.value_names,
-			     this.props.facet.time_series_value_counts[ti]);
-      let maxFacetValue = maxCount(this.props.facet.time_series_value_counts);
-      gridItems.push(
-	<div className={classes.gridItem} key={this.props.facet.time_names[ti]}>
-	  <Grid item>
-	    <div className={classes.histogramPlot}>
-	      <HistogramPlot
-	        es_field_name={tsv_es_field_name}
-	        es_field_type={this.props.facet.es_field_type}
-	        values={values}
- 	        selectedValues={this.props.selectedFacetValues.get(tsv_es_field_name)}
-	        maxFacetValue={maxFacetValue}
-	        updateFacets={this.props.updateFacets}
-	        isTimeSeries={true}
-	        labels={(ti == 0)}
-	        axisName={this.props.facet.name}
-	      />
-	    </div>
-	    <TimeSeriesPlotHeader
-	      name={this.props.facet.time_names[ti]}
-	      values={values}
- 	      selectedValues={this.props.selectedFacetValues.get(tsv_es_field_name)}
-	    />
-	  </Grid>
-	</div>
-      );
+    const baseVegaLiteSpec = {
+      $schema: "https://vega.github.io/schema/vega-lite/v3.json",
+      config: {
+	// Config that applies to both axes go here
+	axis: {
+	  domainColor: colors.gray[4],
+	  gridColor: colors.gray[6],
+	  labelColor: colors.gray[0],
+	  labelFont: "Montserrat",
+	  labelFontWeight: 500,
+	  labelPadding: 9,
+	  ticks: false
+	}
+      },
+      encoding: {
+	color: {
+	  field: "dimmed",
+	  type: "nominal",
+	  scale: {
+            range: [
+              // Default bar color
+              colors.blue[2],
+              // Unselected bar
+              colors.blue[5]
+            ]
+	  },
+	  legend: null
+	},
+	tooltip: {
+	  field: "text",
+	  type: "nominal"
+	},
+	x: {},
+	y: {},
+	column: {},
+	// opacity is needed for creating transparent bars.
+	opacity: {
+	  field: "opaque",
+	  type: "nominal",
+	  scale: {
+            range: [0, 1]
+	  },
+	  legend: null
+	}
+      },
+      mark: {
+	type: "bar",
+	cursor: "pointer"
+      },
+      padding: {
+	left: 0,
+	top: 17,
+	right: 0,
+	bottom: 16
+      },
+      width: 60
+    };
+
+    let facetValueNames = this.props.facet.value_names.slice();
+    const vegaLiteSpec = Object.assign({}, baseVegaLiteSpec);
+    if (!isCategorical(this.props.facet.es_field_type)) {
+      // For numeric facets, higher numbers should be higher on the y-axis
+      facetValueNames.reverse();
     }
+
+    const facetValueNameAxis = {
+      field: "facet_value",
+      type: "nominal",
+      title: this.props.facet.name,
+      sort: facetValueNames,
+      axis: {
+        labelFontSize: 12,
+        labelLimit: facetValueNameWidthLimit
+      },
+      scale: {
+        // Bar height (15px) + whitespace height (10px) = 25px
+        rangeStep: 25,
+        // Proportion of step that is whitespace; 10/25 = .4
+        paddingInner: 0.4,
+        // There should be 5 pixels of whitespace under bottom bar
+        //. 5/25/2 ~= .1
+        paddingOuter: 0.1
+      }
+    };
+
+    const facetValueCountAxis = {
+      axis: {
+	labels: false
+      },
+      field: "count",
+      type: "quantitative",
+      title: "",
+      stack: null
+    };
+
+    const facetValueTimeAxis = {
+      // TODO: add styling here
+      field: "time_series_value",
+      type: "ordinal",
+      title: "Time" // TODO: add name specified in ui.json
+    }
+
+    // Make bars horizontal, to allow for more space for facet value names for
+    // categorical facets.
+    vegaLiteSpec.encoding.x = facetValueCountAxis;
+    vegaLiteSpec.encoding.y = facetValueNameAxis;
+    vegaLiteSpec.encoding.column = facetValueTimeAxis;
+
+    const data = { values: [] };
+    for (let ti = 0; ti < this.props.facet.time_names.length; ti++) {
+      for (let vi = 0; vi < this.props.facet.value_names.length; vi++) {
+	let name = this.props.facet.value_names[vi];
+	let count = this.props.facet.time_series_value_counts[ti][vi];
+	let time = this.props.facet.time_names[ti];
+	data.values.push({
+	  facet_value: name,
+	  count: count,
+	  time_series_value: time,
+	  dimmed: this.isValueDimmed(name, this.props.facet.es_field_name + "." + time),
+	  text: `${name}: ${count}`,
+	  opaque: true
+	});
+      }
+    }
+
+    // Create transparent bar that extends the entire length of the chart. This
+    // makes tooltip/selection easier for facet values that have very low count.
+    let maxFacetValue = maxCount(this.props.facet.time_series_value_counts);
+    data.values = data.values.concat(
+      data.values.map(v => {
+        const invisible = Object.assign({}, v);
+        invisible.opaque = false;
+        invisible.count = maxFacetValue;
+        return invisible;
+      })
+    );
+
+    // vega-lite spec is easier to construct than vega spec. But certain
+    // properties aren't available in vega-lite spec (vega-lite is a subset of
+    // vega). So construct vega-lite spec, compile to vega spec, edit properties
+    // that are only available in vega, then render Vega component.
+    vegaLiteSpec.data = data;
+    const vegaSpec = vl.compile(vegaLiteSpec).spec;
+
+    // Setting align removes whitespace over top bar.
+    // When https://github.com/vega/vega-lite/issues/4741 is fixed, set align
+    // normally.
+    vegaSpec.scales[1].align = 0;
+
+    let vega = (
+      <Vega
+        spec={vegaSpec}
+        tooltip={new Handler({ theme: "custom" }).call}
+        onNewView={this.onNewView}
+      />
+    );
 
     return (
       <div className={classes.timeSeriesHistogramPlot}>
-        <Grid
-	  container
-	  className={classes.grid}
-	  direction="row"
-        >
-          {gridItems}
-        </Grid>
+	<div className={classes.vega}> {vega} </div>
       </div>
     );
+  }
+
+  isValueDimmed(facetValueName, tsv_es_field_name) {
+    let selectedValues = this.props.selectedFacetValues.get(tsv_es_field_name);
+    return (
+      selectedValues != null &&
+      selectedValues.length > 0 &&
+      !selectedValues.includes(facetValueName)
+    );
+  }
+
+  onClick(event, item) {
+    // Ignore clicks which are not located on histogram
+    // bars.
+    if (item && item.datum && item.datum.facet_value) {
+      console.log(item)
+      let tsv_es_field_name = (this.props.facet.es_field_name + "." +
+			       item.datum.time_series_value);
+      let selectedValues = this.props.selectedFacetValues.get(tsv_es_field_name);
+      // facetValue is a string, eg "female"
+      // If bar was clicked, item.datum.facet_value is populated.
+      // If axis label was clicked, item.datum.value is populated.
+      const facetValue =
+        "facet_value" in item.datum ? item.datum.facet_value : item.datum.value;
+      let isSelected;
+      // selectedValues contains what was selected before the click.
+      // isSelected contains if facet value was selected after the click.
+      if (
+        selectedValues != null &&
+        selectedValues.length > 0 &&
+        selectedValues.includes(facetValue)
+      ) {
+        isSelected = false;
+      } else {
+        isSelected = true;
+      }
+
+      this.props.updateFacets(
+        tsv_es_field_name,
+        facetValue,
+        isSelected
+      );
+    }
+  }
+
+  onNewView(view) {
+    view.addEventListener("click", this.onClick);
   }
 }
 
